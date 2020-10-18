c7d307b6c9fd3681d1b0a335ede94fc7f455e44e
Tine Kondo <kondotine@gmail.com>
2020-10-18 03:14:02 +0200

    feat(schematics): add `application` schematics

---
+++ packages/nx-spring-boot/src/schematics/application/lib/index.ts
@@ -0,0 +1,4 @@
+ export { generateSpringBootProject } from './generate-boot-project';
+ export { restoreExecutePermission } from './restore-execute-permission';
+ export { addBuilInfoTask } from './add-build-info-task';
+ export { normalizeOptions } from './normalize-options';
