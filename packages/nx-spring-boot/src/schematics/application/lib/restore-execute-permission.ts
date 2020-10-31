abbe78687cfe07a724846664b720d0623451b8c0
Tine Kondo <kondotine@gmail.com>
2020-10-31 01:20:28 +0100

    refactor(nx-spring-boot): replace `process.cwd()` by `appRootPath` util

--- packages/nx-spring-boot/src/schematics/application/lib/restore-execute-permission.ts
+++ packages/nx-spring-boot/src/schematics/application/lib/restore-execute-permission.ts
@@ -3,6 +3,7 @@ import {
     Rule,
     Tree
 } from '@angular-devkit/schematics';
+import { appRootPath } from '@nrwl/workspace/src/utils/app-root';
 import { NormalizedSchema } from '../schema';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -12,7 +13,7 @@ export function restoreExecutePermission(options: NormalizedSchema): Rule {
     //workaround until issue https://github.com/ZJONSSON/node-unzipper/issues/216 is fixed
     const isWin = process.platform === "win32";
     return isWin ? noop() : (tree: Tree) => {
-        const executable = path.normalize(`${process.cwd()}/${options.projectRoot}/${options.type === 'maven-project' ? 'mvnw' : 'gradlew'}`);
+        const executable = path.normalize(`${appRootPath}/${options.projectRoot}/${options.type === 'maven-project' ? 'mvnw' : 'gradlew'}`);
         fs.chmodSync(executable, 0o755);
         return tree;
     };
