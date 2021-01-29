57769009e51bef21398311b8a44ae555598c75a1
Tine Kondo <kondotine@gmail.com>
2021-01-29 10:14:27 +0100

    test(nx-spring-boot): fix builders tests

--- packages/nx-spring-boot/src/builders/clean/builder.spec.ts
+++ packages/nx-spring-boot/src/builders/clean/builder.spec.ts
@@ -13,8 +13,16 @@ const options: CleanBuilderSchema = {
 describe('Command Runner Builder', () => {
   let architect: Architect;
   let architectHost: TestingArchitectHost;
+  let fileExists: jest.Mock<any>;
 
   beforeEach(async () => {
+    jest.resetModules();
+
+    fileExists = jest.fn();
+    jest.doMock('@nrwl/workspace/src/utils/fileutils', () => ({
+      fileExists,
+    }));
+
     const registry = new schema.CoreSchemaRegistry();
     registry.addPostTransform(schema.transforms.addUndefinedDefaults);
 
@@ -26,7 +34,29 @@ describe('Command Runner Builder', () => {
     await architectHost.addBuilderFromPackage(join(__dirname, '../../..'));
   });
 
-  it('can run', async () => {
+  it('can run maven build ', async () => {
+    fileExists.mockImplementation((path: string) => path.indexOf('pom.xml') !== -1)
+
+    // A "run" can have multiple outputs, and contains progress information.
+    const clean = await architect.scheduleBuilder(
+      '@nxrocks/nx-spring-boot:clean',
+      options
+    );
+    // The "result" member (of type BuilderOutput) is the next output.
+    const output = await clean.result;
+
+    // Stop the builder from running. This stops Architect from keeping
+    // the builder-associated states in memory, since builders keep waiting
+    // to be scheduled.
+    await clean.stop();
+
+    // Expect that it succeeded.
+    expect(output.success).toBe(true);
+  });
+
+  it('can run gradle build ', async () => {
+    fileExists.mockImplementation((path: string) => path.indexOf('build.gradle') !== -1)
+
     // A "run" can have multiple outputs, and contains progress information.
     const clean = await architect.scheduleBuilder(
       '@nxrocks/nx-spring-boot:clean',
