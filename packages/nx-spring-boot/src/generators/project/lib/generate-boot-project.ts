fba56e62b5ff5c66e0756b170730e02495a9c43f
Tine Kondo <kondotine@gmail.com>
2021-05-19 08:38:24 +0200

    refactor(nx-spring-boot): extract generated zip files directly
    - This removes the need for the additionnal `restoreExecutePermission` step on the maven/gradle wrappers
    - This will ease the migration towards `Tree.write()`
    - Closes #13

--- packages/nx-spring-boot/src/generators/project/lib/generate-boot-project.ts
+++ packages/nx-spring-boot/src/generators/project/lib/generate-boot-project.ts
@@ -2,9 +2,12 @@ import { Tree, logger } from '@nrwl/devkit';
 import { appRootPath } from '@nrwl/workspace/src/utils/app-root';
 
 import fetch from 'node-fetch';
-import { Extract } from 'unzipper';
+import { Entry, Parse } from 'unzipper';
 import { NormalizedSchema } from '../schema';
 import { getPackageLatestNpmVersion, buildBootDownloadUrl } from '../../../utils/boot-utils';
+
+import * as fs from 'fs';
+import * as path from 'path';
 
 export async function generateBootProject(tree: Tree, options: NormalizedSchema): Promise<void> {
     const downloadUrl = buildBootDownloadUrl(options);
@@ -23,20 +26,22 @@ export async function generateBootProject(tree: Tree, options: NormalizedSchema)
 
     logger.info(`Extracting Spring Boot project zip to '${appRootPath}/${options.projectRoot}'...`);
 
-    return response.body.pipe(Extract({ path: options.projectRoot })).promise();
-    /*
-    return response.body.pipe(Parse({forceStream: true}))
-        .on('entry', async (entry: Entry)=>{
-            if( entry.type === 'Directory'){
-                return entry.autodrain();
-            }
+    const zip = response.body.pipe(Parse({ forceStream: true }));
+    for await (const entry of zip) {
+        const e = entry as Entry;
+        const filePath = e.path;
+        const fileContent = await e.buffer();
+        const fileType = e.type; // 'Directory' or 'File'
+        if(fileType === 'File'){
+            const filePermissions = filePath.endsWith('mvnw') || filePath.endsWith('gradlew') ? 0o755: undefined;
+            // once issue  https://github.com/nrwl/nx/issues/5680 is accepted/merged use this instead
+            //tree.write(`${options.projectRoot}/${filePath}`, fileContent, filePermissions);
 
-            const fileName = entry.path;
-            const fileContent = await entry.buffer();
-            logger.info(`Processing zip file entry '${fileName}'...`);
-            tree.write(`${options.projectRoot}/${fileName}`, fileContent);
-            // entry.pipe(process.stdout);
-        })
-        .promise();
-    */
+            const fullFilePath = path.join(appRootPath, options.projectRoot,filePath);
+            fs.mkdirSync(path.dirname(fullFilePath), { recursive: true});
+            fs.writeFileSync(fullFilePath, fileContent);
+            if(filePermissions)
+                fs.chmodSync(fullFilePath, filePermissions);
+        }
+    }
 }
