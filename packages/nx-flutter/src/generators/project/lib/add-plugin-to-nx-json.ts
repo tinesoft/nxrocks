9b84abc62bce8ca843db33635ad0a626e1b184e0
Andreas Abegg <andreas.abegg@apmatic.ch>
2021-07-13 09:47:18 +0200

    fix(nx-flutter): add plugin to nx.json only if not included already

--- packages/nx-flutter/src/generators/project/lib/add-plugin-to-nx-json.ts
+++ packages/nx-flutter/src/generators/project/lib/add-plugin-to-nx-json.ts
@@ -3,7 +3,10 @@ import { Tree, readJson, writeJson} from '@nrwl/devkit';
 export function addPluginToNxJson(tree:Tree){
     const nxJson = readJson(tree, 'nx.json');
     nxJson.plugins = (nxJson.plugins || []);
-    nxJson.plugins.push('@nxrocks/nx-flutter');
+
+    if (!nxJson.plugins.includes('@nxrocks/nx-flutter')) {
+      nxJson.plugins.push('@nxrocks/nx-flutter');
+    }
 
     writeJson(tree,'nx.json', nxJson);
 }
