15511ae8ebdbc4d8cd412afd266457cf08b14f72
Tine Kondo <kondotine@gmail.com>
2021-08-12 21:51:58 +0200

    fix(nx-spring-boot): add plugin to nx.json only if not included already

--- packages/nx-spring-boot/src/generators/project/lib/add-plugin-to-nx-json.ts
+++ packages/nx-spring-boot/src/generators/project/lib/add-plugin-to-nx-json.ts
@@ -3,7 +3,9 @@ import { Tree, readJson, writeJson} from '@nrwl/devkit';
 export function addPluginToNxJson(tree:Tree){
     const nxJson = readJson(tree, 'nx.json');
     nxJson.plugins = (nxJson.plugins || []);
-    nxJson.plugins.push('@nxrocks/nx-spring-boot');
+    if (!nxJson.plugins.includes('@nxrocks/nx-spring-boot')) {
+        nxJson.plugins.push('@nxrocks/nx-spring-boot');
+    }
 
     writeJson(tree,'nx.json', nxJson);
 }
