1a39ec45c6083c68462c8498090734251c4564b2
Tine Kondo <kondotine@gmail.com>
2022-02-08 08:12:46 +0100

    refactor(nx-spring-boot): improve readability by using `stripIndent`

--- packages/nx-spring-boot/src/generators/project/lib/add-build-info-task.ts
+++ packages/nx-spring-boot/src/generators/project/lib/add-build-info-task.ts
@@ -2,20 +2,22 @@ import {
     logger,
     Tree
 } from '@nrwl/devkit';
+import { stripIndent } from '@nxrocks/common';
 import { NormalizedSchema } from '../schema';
 
 export function addBuilInfoTask(tree: Tree, options: NormalizedSchema) {
     if (options.projectType === 'application' && options.buildSystem === 'gradle-project') {
         logger.debug(`Adding 'buildInfo' task to the build.gradle file...`);
 
-        const buildInfoTask = `
-springBoot {
-    buildInfo()
-}
-  `;
+        const buildInfoTask =
+        stripIndent`
+        springBoot {
+        	buildInfo()
+        }
+        `;
         const ext = options.language === 'kotlin' ? '.kts' : ''
         const buildGradlePath = `${options.projectRoot}/build.gradle${ext}`;
-        const content = tree.read(buildGradlePath).toString() + buildInfoTask;
+        const content = tree.read(buildGradlePath, 'utf-8') + buildInfoTask;
         tree.write(buildGradlePath, content);
     }
 }
--- packages/nx-spring-boot/src/generators/project/lib/disable-boot-jar-task.ts
+++ packages/nx-spring-boot/src/generators/project/lib/disable-boot-jar-task.ts
@@ -2,23 +2,26 @@ import {
     logger,
     Tree
 } from '@nrwl/devkit';
+import { stripIndent } from '@nxrocks/common';
 import { NormalizedSchema } from '../schema';
 
 export function disableBootJarTask(tree: Tree, options: NormalizedSchema) {
     if (options.projectType === 'library' && options.buildSystem === 'gradle-project') {
         logger.debug(`Disabling 'bootJar' task on a library project...`);
 
-        const bootJarDisabledTask = `
-// spring boot library projects don't need an executable jar, so we disable it
-bootJar {
-    enabled = false
-}
-`;
-        const jarEnabledTask = `
-jar {
-    enabled = true
-}
-`;
+        const bootJarDisabledTask = 
+        stripIndent`
+        // spring boot library projects don't need an executable jar, so we disable it
+        bootJar {
+        	enabled = false
+        }
+        `;
+        const jarEnabledTask = 
+        stripIndent`
+        jar {
+        	enabled = true
+        }
+        `;
         const ext = options.language === 'kotlin' ? '.kts' : ''
         const buildGradlePath = `${options.projectRoot}/build.gradle${ext}`;
         let content = tree.read(buildGradlePath).toString();
