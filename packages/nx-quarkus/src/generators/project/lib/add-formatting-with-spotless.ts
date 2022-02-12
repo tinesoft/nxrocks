59cb3728aaefcf658ef17fa4666e552b66e7d69c
Tine Kondo <kondotine@gmail.com>
2022-02-12 10:32:00 +0100

    fix(nx-quarkus): remove `ratchetFrom` from the default Spotless config

--- packages/nx-quarkus/src/generators/project/lib/add-formatting-with-spotless.ts
+++ packages/nx-quarkus/src/generators/project/lib/add-formatting-with-spotless.ts
@@ -1,5 +1,4 @@
 import {
-    readJson,
     Tree
 } from '@nrwl/devkit';
 import { addSpotlessGradlePlugin, addSpotlessMavenPlugin } from '@nxrocks/common';
@@ -7,14 +6,11 @@ import { NormalizedSchema } from '../schema';
 
 export function addFormattingWithSpotless(tree: Tree, options: NormalizedSchema) {
 
-    const nxJson = readJson(tree, 'nx.json');
-    const gitBaseBranch = nxJson.affected?.defaultBase || 'master';
-
     if (options.buildSystem === 'MAVEN') {
-        addSpotlessMavenPlugin(tree, options.projectRoot, 'java', 11, gitBaseBranch);
+        addSpotlessMavenPlugin(tree, options.projectRoot, 'java', 11);
     }
     else {
-        addSpotlessGradlePlugin(tree, options.projectRoot, 'java', 11, gitBaseBranch, options.buildSystem === 'GRADLE_KOTLIN_DSL');
+        addSpotlessGradlePlugin(tree, options.projectRoot, 'java', 11, null, options.buildSystem === 'GRADLE_KOTLIN_DSL');
     }
 
 }
