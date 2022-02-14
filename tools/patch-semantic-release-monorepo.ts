795e87b4fabdc5c08bbd43636fb94bbd2cce5721
Tine Kondo <kondotine@gmail.com>
2022-02-14 18:11:17 +0100

    chore: patch `semantic-release-monorepo` to include scopless commits

---
+++ tools/patch-semantic-release-monorepo.ts
@@ -0,0 +1,17 @@
+import { appRootPath } from '@nrwl/tao/src/utils/app-root';
+
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+   
+// patch the check from `semantic-release-monorepo` to also include scopeless commits 
+const fileToPatch = 'node_modules/semantic-release-monorepo/src/only-package-commits.js';
+const onlyPackageCommitsJs = readFileSync(join(appRootPath, fileToPatch), 'utf8');
+const patchedFile = onlyPackageCommitsJs
+.replace('    if (packageFile) {',`
+    const isScopelessCommit = /^[a-z]+\:/.test(subject);
+    if (packageFile || isScopelessCommit) {`)
+.replace(`Including commit "%s" because it modified package file "%s".`,`Including commit "%s" either because it has no scope or it modified package file "%s".`)
+.replace(`    return !!packageFile;`,`    return !!packageFile || isScopelessCommit;`);
+
+writeFileSync(join(appRootPath, fileToPatch), patchedFile);
