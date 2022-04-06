f6b433d458b0f94c297b4581cd18f0bef3c0c5e6
Tine Kondo <kondotine@gmail.com>
2022-04-07 00:55:01 +0200

    fix(nx-spring-boot): fix `Premature close` error when generating project
    Closes #97

--- packages/nx-spring-boot/src/generators/project/lib/generate-boot-project.ts
+++ packages/nx-spring-boot/src/generators/project/lib/generate-boot-project.ts
@@ -2,10 +2,9 @@ import { Tree, logger } from '@nrwl/devkit';
 import { appRootPath } from '@nrwl/workspace/src/utils/app-root';
 
 import fetch from 'node-fetch';
-import { Entry, Parse } from 'unzipper';
 import { NormalizedSchema } from '../schema';
 import { buildBootDownloadUrl } from '../../../utils/boot-utils';
-import { getPackageLatestNpmVersion, NX_SPRING_BOOT_PKG } from '@nxrocks/common';
+import { extractFromZipStream, getPackageLatestNpmVersion, NX_SPRING_BOOT_PKG } from '@nxrocks/common';
 
 export async function generateBootProject(tree: Tree, options: NormalizedSchema): Promise<void> {
     const downloadUrl = buildBootDownloadUrl(options);
@@ -23,15 +22,8 @@ export async function generateBootProject(tree: Tree, options: NormalizedSchema)
 
     logger.info(`Extracting Spring Boot project zip to '${appRootPath}/${options.projectRoot}'...`);
 
-    const zip = response.body.pipe(Parse({ forceStream: true }));
-    for await (const entry of zip) {
-        const e = entry as Entry;
-        const filePath = e.path;
-        const fileContent = await e.buffer();
-        const fileType = e.type; // 'Directory' or 'File'
-        if(fileType === 'File'){
-            const execPermission = filePath.endsWith('mvnw') || filePath.endsWith('gradlew') ? '755': undefined;
-            tree.write(`${options.projectRoot}/${filePath}`, fileContent, {mode: execPermission});
-        }
-    }
+    await extractFromZipStream(response.body, (entryPath, entryContent) => {
+        const execPermission = entryPath.endsWith('mvnw') || entryPath.endsWith('gradlew') ? '755': undefined;
+        tree.write(`${options.projectRoot}/${entryPath}`, entryContent, {mode: execPermission});
+    });
 }
