565ae1d9a23f30583988dac65b33f43fbc5071f4
Tine Kondo <kondotine@gmail.com>
2022-04-07 00:55:01 +0200

    fix(nx-quarkus): fix `Premature close` error when generating project
    Closes #97

--- packages/nx-quarkus/src/generators/project/lib/generate-quarkus-project.ts
+++ packages/nx-quarkus/src/generators/project/lib/generate-quarkus-project.ts
@@ -2,10 +2,9 @@ import { Tree, logger } from '@nrwl/devkit';
 import { appRootPath } from '@nrwl/workspace/src/utils/app-root';
 
 import fetch from 'node-fetch';
-import { Entry, Parse } from 'unzipper';
 import { NormalizedSchema } from '../schema';
 import {  buildQuarkusDownloadUrl } from '../../../utils/quarkus-utils';
-import { getPackageLatestNpmVersion, NX_QUARKUS_PKG } from '@nxrocks/common';
+import { extractFromZipStream, getPackageLatestNpmVersion, NX_QUARKUS_PKG } from '@nxrocks/common';
 
 export async function generateQuarkusProject(tree: Tree, options: NormalizedSchema): Promise<void> {
     const downloadUrl = buildQuarkusDownloadUrl(options);
@@ -23,15 +22,9 @@ export async function generateQuarkusProject(tree: Tree, options: NormalizedSche
 
     logger.info(`📦 Extracting Quarkus project zip to '${appRootPath}/${options.projectRoot}'...`);
 
-    const zip = response.body.pipe(Parse({ forceStream: true }));
-    for await (const entry of zip) {
-        const e = entry as Entry;
-        const filePath = e.path.replace(`${options.artifactId}/`,''); // remove the inner folder in the zip
-        const fileContent = await e.buffer();
-        const fileType = e.type; // 'Directory' or 'File'
-        if(fileType === 'File'){
-            const execPermission = filePath.endsWith('mvnw') || filePath.endsWith('gradlew') ? '755': undefined;
-            tree.write(`${options.projectRoot}/${filePath}`, fileContent, {mode: execPermission});
-        }
-    }
+    await extractFromZipStream(response.body, (entryPath, entryContent) => {
+        const filePath = entryPath.replace(`${options.artifactId}/`,''); // remove the inner folder in the zip
+        const execPermission = filePath.endsWith('mvnw') || filePath.endsWith('gradlew') ? '755': undefined;
+        tree.write(`${options.projectRoot}/${filePath}`, entryContent, {mode: execPermission});
+    });
 }
