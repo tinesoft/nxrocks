4d0a8f5db66e4e06d1f2c22472f4b5357196a762
Tine Kondo <kondotine@gmail.com>
2022-06-24 07:02:44 +0000

    feat(nx-spring-boot): check if source is a valid boot project on linking

--- packages/nx-spring-boot/src/generators/link/generator.spec.ts
+++ packages/nx-spring-boot/src/generators/link/generator.spec.ts
@@ -3,6 +3,9 @@ import { Tree, readProjectConfiguration, addProjectConfiguration } from '@nrwl/d
 
 import generator from './generator';
 import { LinkGeneratorSchema } from './schema';
+import { isBootProject } from '../../utils/boot-utils';
+
+jest.mock('../../utils/boot-utils');
 
 describe('link generator', () => {
   let tree: Tree;
@@ -20,11 +23,19 @@ describe('link generator', () => {
       sourceRoot: `apps/${options.targetProjectName}/src`,
       root: `apps/${options.targetProjectName}`,
     });
+
+    jest.resetAllMocks();
   });
 
   it('should run successfully', async () => {
+    // mock the function that checks if the project is a Spring-Boot project
+    (isBootProject as jest.Mock).mockImplementation(() => true);
     await generator(tree, options);
     const targetProject = readProjectConfiguration(tree, options.targetProjectName);
     expect(targetProject.implicitDependencies).toEqual([options.sourceProjectName]);
+  });
+
+  it('should fail if source project is not a Spring boot project', async () => {
+    await expect(generator(tree, options)).rejects.toThrow(`The source project (1st argument of this 'link' generator) must be a Spring-Boot project`) ;
   })
 });
--- packages/nx-spring-boot/src/project-graph.ts
+++ packages/nx-spring-boot/src/project-graph.ts
@@ -1,23 +1,10 @@
 import {
     ProjectGraph,
-    ProjectGraphProcessorContext,
-    ProjectConfiguration} from '@nrwl/devkit';
+    ProjectGraphProcessorContext} from '@nrwl/devkit';
 
 
-import { isMavenProject, checkProjectBuildFileContains, isGradleProject, getProjectGraph, NX_SPRING_BOOT_PKG, getJvmPackageInfo } from '@nxrocks/common';
-
-function isBootProject(project: ProjectConfiguration): boolean {
-    
-    if(isMavenProject(project)) {
-        return checkProjectBuildFileContains(project, { fragments: ['<artifactId>spring-boot-starter-parent</artifactId>']}) ;
-    }
-
-    if(isGradleProject(project)) {
-        return checkProjectBuildFileContains(project, { fragments: ['implementation \'org.springframework.boot:spring-boot-starter-parent\'', 'implementation("org.springframework.boot:spring-boot-starter")']}) ;
-    }
-
-    return false;
-}
+import { getProjectGraph, NX_SPRING_BOOT_PKG, getJvmPackageInfo } from '@nxrocks/common';
+import { isBootProject } from './utils/boot-utils';
 
 export function processProjectGraph(
     graph: ProjectGraph,
