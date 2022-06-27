f4265757aad0a350b3b966f0076192600221ae67
Tine Kondo <kondotine@gmail.com>
2022-06-27 21:10:35 +0000

    fix(common): ignore output when fetching package latest version from npm

--- packages/common/src/lib/core/utils/utils.ts
+++ packages/common/src/lib/core/utils/utils.ts
@@ -2,7 +2,9 @@ import { execSync } from 'child_process';
 
 export function getPackageLatestNpmVersion(pkg: string): string {
   try {
-    return execSync(`npm show ${pkg} version`).toString().trim() || 'latest';
+    return execSync(`npm show ${pkg} version`, {
+      stdio: 'ignore',
+    }).toString().trim() || 'latest';
   } catch (e) {
     return 'latest';
   }
