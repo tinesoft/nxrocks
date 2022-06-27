e59c29b95e082bbbac20dc993531c09a0493d372
Tine Kondo <kondotine@gmail.com>
2022-06-27 20:03:41 +0000

    feat(common): add support for `nx-micronaut` plugin

--- packages/common/src/lib/core/constants.ts
+++ packages/common/src/lib/core/constants.ts
@@ -9,5 +9,8 @@ export const NX_QUARKUS_PKG = '@nxrocks/nx-quarkus';
 export const NX_SPRING_BOOT = 'nx-spring-boot';
 export const NX_SPRING_BOOT_PKG = '@nxrocks/nx-spring-boot';
 
+export const NX_MICRONAUT = 'nx-micronaut';
+export const NX_MICRONAUT_PKG = '@nxrocks/nx-micronaut';
+
 export const COMMON = 'common';
 export const COMMON_PKG = '@nxrocks/common';
