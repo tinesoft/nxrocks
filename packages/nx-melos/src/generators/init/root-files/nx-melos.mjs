6c7cff6c81740bb9384ecaa7f1bbaecd5fd611be
Tine Kondo <kondotine@gmail.com>
2023-01-14 15:02:55 +0000

    fix(nx-melos): fix typo in `nx-melos.mjs` file

--- packages/nx-melos/src/generators/init/root-files/nx-melos.mjs
+++ packages/nx-melos/src/generators/init/root-files/nx-melos.mjs
@@ -1,9 +1,9 @@
 import { homedir} from 'os';
 import { spawnSync} from 'child_process';
 
-// This plugin (nx-melos) installs 'melos' (upon inititialization) as a Dart global package (inside user home folder at `~/.pub-cache/bin`).
-// But that folder might not be on your PATH environment variable, so we can not just rely on the bare 'melos' executable being available globally. 
-// Thus this wrapper, that uses the full path (OS independently) to the 'melos' executable.
+// This plugin (nx-melos) installs 'melos' (upon initialization) as a Dart global package (inside user's home folder at `~/.pub-cache/bin`).
+// But that folder might not be on your PATH environment variable, so we cannot rely on the 'melos' executable to be globally available. 
+// Thus this wrapper, that uses the full path (OS independent) to the 'melos' executable.
 
 const melosExec = `${homedir()}/.pub-cache/bin/melos`;
 
