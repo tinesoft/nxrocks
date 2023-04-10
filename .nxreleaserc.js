d9388dad1c79026d59019fe13e75dd28d3ecb404
Cirrus CI <support@cirruslabs.org>
2023-04-10 18:07:54 +0000

    chore(release): simplify `nx-semantic-release`  plugin configuration

--- .nxreleaserc.js
+++ .nxreleaserc.js
@@ -1,35 +1,11 @@
 
-const formatFile = file => `nx format:write --files ${file}`;
-const copyFile = (file, dest) => `cp ${file} ${dest}`;
-
 module.exports = {
   changelog: true,
   npm: true,
   github: true,
   repositoryUrl: "https://github.com/tinesoft/nxrocks",
-  branches: [
-    'master',
-    'main',
-    'next',
-    { name: 'beta', prerelease: true },
-    { name: 'alpha', prerelease: true },
-  ],
   commitMessage: "chore(release): 🚀 release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
   tagFormat: "${PROJECT_NAME}/v${VERSION}",
-  plugins: [
-    [
-      '@semantic-release/exec',
-      {
-        prepareCmd: [
-          formatFile('packages/${PROJECT_NAME}/CHANGELOG.md'),
-          copyFile('packages/${PROJECT_NAME}/CHANGELOG.md', 'dist/packages/${PROJECT_NAME}'),
-          copyFile('packages/${PROJECT_NAME}/README.md', 'dist/packages/${PROJECT_NAME}'),
-          copyFile(`LICENSE`, 'dist/packages/${PROJECT_NAME}'),
-        ].join(' && '),
-        execCwd: '${WORKSPACE_DIR}'
-      },
-    ],
-  ],
   buildTarget: "${PROJECT_NAME}:build",
   outputPath: '${WORKSPACE_DIR}/dist/packages/${PROJECT_NAME}'
 }
