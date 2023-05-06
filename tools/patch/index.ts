c0e6035babb6b89b73c360396b3d1ce4df39de89
Tine Kondo <kondotine@gmail.com>
2023-05-06 02:41:31 +0000

    chore: update to Nx workspace `v16.1.1`

--- tools/patch/index.ts
+++ tools/patch/index.ts
@@ -1,7 +1,7 @@
-import { patchEnquirer } from "./patch-enquirer";
-import { patchNxJsDeps } from "./patch-nx-js-deps";
+import { patchEnquirer } from './patch-enquirer';
+import { patchNxJsDeps } from './patch-nx-js-deps';
 
-if(!process.env.SKIP_PATCHES){
-    patchNxJsDeps();
-    patchEnquirer();
+if (!process.env.SKIP_PATCHES) {
+  patchNxJsDeps();
+  patchEnquirer();
 }
