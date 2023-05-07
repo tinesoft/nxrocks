89063fdda05c8d9dd599d62c04d5dae609d9d782
Tine Kondo <kondotine@gmail.com>
2023-05-07 05:14:03 +0000

    chore: fix `patchNxProjectJs`

--- tools/patch/index.ts
+++ tools/patch/index.ts
@@ -1,7 +1,9 @@
 import { patchEnquirer } from './patch-enquirer';
 import { patchNxJsDeps } from './patch-nx-js-deps';
+import { patchNxProjectJs } from './patch-nx-project';
 
 if (!process.env.SKIP_PATCHES) {
   patchNxJsDeps();
+  patchNxProjectJs();
   patchEnquirer();
 }
--- tools/patch/patch-nx-project.ts
+++ tools/patch/patch-nx-project.ts
@@ -1,14 +1,25 @@
-
 import { workspaceRoot } from '@nx/devkit';
 import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
 export function patchNxProjectJs() {
-    // activate log in ensureNxProject() call hierarchy
-    const nxProjectJs = readFileSync(join(workspaceRoot, 'node_modules/@nx/nx-plugin/src/utils/testing-utils/nx-project.js'), 'utf8');
-    const patchedNxProjectJs = nxProjectJs
-        .replace('silent && false', 'silent')
-        .replace('silent = true', 'silent = false')
-        .replace(`runNxNewCommand('', true)`, `runNxNewCommand('', false)`);
-    writeFileSync(join(workspaceRoot, 'node_modules/@nx/nx-plugin/src/utils/testing-utils/nx-project.js'), patchedNxProjectJs);
+  // activate log in ensureNxProject() call hierarchy
+  const nxProjectJs = readFileSync(
+    join(
+      workspaceRoot,
+      'node_modules/@nx/plugin/src/utils/testing-utils/nx-project.js'
+    ),
+    'utf8'
+  );
+  const patchedNxProjectJs = nxProjectJs
+    .replace('silent && false', 'silent')
+    .replace('silent = true', 'silent = false')
+    .replace(`runNxNewCommand('', true)`, `runNxNewCommand('', false)`);
+  writeFileSync(
+    join(
+      workspaceRoot,
+      'node_modules/@nx/plugin/src/utils/testing-utils/nx-project.js'
+    ),
+    patchedNxProjectJs
+  );
 }
