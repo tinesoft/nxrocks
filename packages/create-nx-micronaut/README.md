2dce94402adb75213e185eaf778ef2c254f5561f
Tine Kondo <kondotine@gmail.com>
2023-10-03 09:17:39 +0000

    feat(create-nx-micronaut): add custom CLI to create Micronaut projects

---
+++ packages/create-nx-micronaut/README.md
@@ -0,0 +1,29 @@
+# create-nx-micronaut 
+
+> Our very own **CLI** to create [Nx](https://nx.dev) workspaces with built-in support for [Micronaut](https://micronaut.io)!
+
+The goal of this CLI is to ease the process of creating a Nx workspace that can host and manage Micronaut projects, thanks to our [@nxrocks/nx-micronaut](https://github.com/tinesoft/nxrocks/blob/develop/packages/nx-micronaut) plugin, that is automatically installed within it.
+
+##  Prerequisites
+
+To run this CLI, all you need is to have [NodeJS](https://nodejs.org/en/download) installed (preferably the current LTS version).
+Then to later create and run your Micronaut projets, you'll need a Java Runtime as well.
+
+- **NodeJS** (preferably latest or LTS version) in path
+- **JRE** or **JDK** in path
+
+## Getting Started
+
+Run `npx create-nx-micronaut your-workspace-name` to execute the CLI.
+
+This command will guide you through the creation process steps by steps.
+
+## Options
+
+Run `npx create-nx-micronaut --help` to print some helpful info on available options.
+
+
+## License
+
+Copyright (c) 2023-present Tine Kondo. Licensed under the MIT License (MIT)
+
---
+++ packages/nx-micronaut/src/generators/preset/generator.spec.ts
@@ -0,0 +1,56 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nx/devkit';
+
+import { presetGenerator } from './generator';
+import { PresetGeneratorSchema } from './schema';
+
+import { Readable } from 'stream';
+
+//mock 'node-fetch' to avoid making the actual call to Spring Initializer
+jest.mock('node-fetch');
+import fetch from 'node-fetch';
+const { Response } = jest.requireActual('node-fetch');
+
+import { mockZipStream } from '@nxrocks/common/testing';
+import { DEFAULT_MICRONAUT_LAUNCH_URL } from '../../utils/micronaut-utils';
+import { POM_XML } from '../project/generator.spec';
+
+describe('preset generator', () => {
+  let tree: Tree;
+  const options: PresetGeneratorSchema = {
+    prjName: 'mnapp',
+    projectType: 'default',
+    basePackage: 'com.tinesoft',
+    buildSystem: 'MAVEN',
+    micronautLaunchUrl: DEFAULT_MICRONAUT_LAUNCH_URL,
+  };
+
+  const mockedFetch = fetch as jest.MockedFunction<typeof fetch>;
+  const mockedResponse = new Response(Readable.from(['micronaut.zip']));
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace();
+    jest.spyOn(mockedResponse.body, 'pipe').mockReturnValue(mockZipStream([]));
+    mockedFetch.mockResolvedValue(mockedResponse);
+
+    const zipFiles = [
+      { filePath: 'pom.xml', fileContent: POM_XML },
+      'mvnw',
+      'README.md',
+    ];
+    // mock the zip content returned by the real call to Spring Initializer
+    jest
+      .spyOn(mockedResponse.body, 'pipe')
+      .mockReturnValue(mockZipStream(zipFiles));
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should run successfully', async () => {
+    await presetGenerator(tree, options);
+    const config = readProjectConfiguration(tree, 'mnapp');
+    expect(config).toBeDefined();
+  });
+});
