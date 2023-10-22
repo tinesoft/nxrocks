de11795fe87ba4a6bef6c4dfabc574db54ae30f5
Tine Kondo <kondotine@gmail.com>
2023-10-22 14:28:16 +0000

    feat(nx-spring-boot): add migration to add `install` target in cacheable operations

---
+++ packages/nx-spring-boot/src/migrations/add-install-to-cacheable-operations.spec.ts
@@ -0,0 +1,19 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { NxJsonConfiguration, Tree, readJson } from '@nx/devkit';
+
+import update from './add-install-to-cacheable-operations';
+
+describe('add-install-to-cacheable-operations migration', () => {
+  let tree: Tree;
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await update(tree);
+    const nxJson = readJson<NxJsonConfiguration>(tree, 'nx.json');
+    expect(nxJson.targetDefaults['install'].cache).toBe(true)
+
+  });
+});
