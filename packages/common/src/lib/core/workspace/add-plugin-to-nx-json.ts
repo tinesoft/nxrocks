0036811d45d177bbcc9254f6378ab5b3a811d60c
Tine Kondo <kondotine@gmail.com>
2023-10-22 06:45:49 +0000

    feat(common): allow setting cacheable operations when add the plugin to `nx.json`

--- packages/common/src/lib/core/workspace/add-plugin-to-nx-json.ts
+++ packages/common/src/lib/core/workspace/add-plugin-to-nx-json.ts
@@ -1,10 +1,16 @@
-import { Tree, readJson, writeJson } from '@nx/devkit';
+import { NxJsonConfiguration, Tree, readJson, writeJson } from '@nx/devkit';
 
-export function addPluginToNxJson(pluginName: string, tree: Tree) {
-  const nxJson = readJson(tree, 'nx.json');
+export function addPluginToNxJson(pluginName: string, tree: Tree, ...cacheableOperations: string[]) {
+  const nxJson = readJson<NxJsonConfiguration>(tree, 'nx.json');
   nxJson.plugins = nxJson.plugins || [];
   if (!nxJson.plugins.includes(pluginName)) {
     nxJson.plugins.push(pluginName);
+  }
+
+  nxJson.targetDefaults ??= {};
+  for (const target of cacheableOperations) {
+    nxJson.targetDefaults[target] ??= {};
+    nxJson.targetDefaults[target].cache ??= true;
   }
 
   writeJson(tree, 'nx.json', nxJson);
