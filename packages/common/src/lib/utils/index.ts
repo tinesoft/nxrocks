1a576348e4adcd131f39b46a102675dd0bb5a95f
Tine Kondo <kondotine@gmail.com>
2023-12-17 10:15:39 +0000

    chore(common): add a `migration-utils` to share common migration code

--- packages/common/src/lib/utils/index.ts
+++ packages/common/src/lib/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './utils';
 export * from './strings-utils';
 export * from './zip-utils';
+export * from './migration-utils';
---
+++ packages/common/src/lib/utils/migration-utils.ts
@@ -0,0 +1,28 @@
+import { NxJsonConfiguration, ProjectConfiguration, Tree, getProjects, readNxJson, updateNxJson, updateProjectConfiguration } from "@nx/devkit";
+
+export async function updateProjectConfigurationIf(tree: Tree, predicate: (project: ProjectConfiguration) => boolean, updater: (project: ProjectConfiguration) => void) {
+
+    const projects = getProjects(tree);
+    for (const [, project] of projects) {
+        if (!project || !predicate(project)) {
+            continue;
+        }
+
+        updater(project);
+
+        updateProjectConfiguration(tree, project.name, project);
+    }
+}
+
+export async function updateNxJsonIf(tree: Tree, predicate: (nxJson: NxJsonConfiguration) => boolean, updater: (project: NxJsonConfiguration) => void) {
+
+    // update options from nx.json target defaults
+    const nxJson = readNxJson(tree);
+    if (!nxJson || !predicate(nxJson)) {
+        return;
+    }
+
+    updater(nxJson);
+
+    updateNxJson(tree, nxJson);
+}
