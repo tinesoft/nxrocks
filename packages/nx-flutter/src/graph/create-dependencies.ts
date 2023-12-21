2085e613bc5e01f0939012bfb160bbdd579f7726
Tine Kondo <kondotine@gmail.com>
2023-12-21 05:29:25 +0000

    feat(nx-flutter): migrate Project Graph generation to `v2` API

---
+++ packages/nx-flutter/src/graph/create-dependencies.ts
@@ -0,0 +1,9 @@
+import { CreateDependencies } from "@nx/devkit";
+
+import { createDependenciesIf } from "@nxrocks/common";
+import { NX_FLUTTER_PKG } from "../index";
+import { isFlutterProject } from "../utils/flutter-utils";
+import { getPackageInfo } from '../utils/deps-utils';
+
+
+export const createDependencies: CreateDependencies = (_, ctx) => createDependenciesIf(NX_FLUTTER_PKG, ['pubspec.yaml'], isFlutterProject, getPackageInfo, ctx);
--- packages/nx-flutter/src/index.ts
+++ packages/nx-flutter/src/index.ts
@@ -1,4 +1,8 @@
-export { projectGenerator } from './generators/project/generator';
-export { processProjectGraph } from './project-graph';
-
 export const NX_FLUTTER_PKG = '@nxrocks/nx-flutter';
+
+export { projectGenerator } from './generators/project/generator';
+export { processProjectGraph } from './graph/process-project-graph';
+export { createDependencies } from './graph/create-dependencies';
+import { createNodesFn } from './graph/create-nodes';
+
+export const createNodes = createNodesFn();
