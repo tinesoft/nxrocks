7d94457b244c6ab058e8541f29252b5917ad92e6
Tine Kondo <kondotine@gmail.com>
2023-12-21 01:13:25 +0000

    feat(nx-spring-boot): migrate Project Graph generation to `v2` API

---
+++ packages/nx-spring-boot/src/graph/create-nodes.ts
@@ -0,0 +1,5 @@
+import { JVM_PROJECT_FILES, createNodesFor } from "@nxrocks/common-jvm";
+import { NX_SPRING_BOOT_PKG } from "../index";
+
+// wrapped iton a () to avoid the 'Cannot access 'NX_SPRING_BOOT_PKG' before initialization'
+export const createNodesFn = () => createNodesFor(JVM_PROJECT_FILES, NX_SPRING_BOOT_PKG); 
