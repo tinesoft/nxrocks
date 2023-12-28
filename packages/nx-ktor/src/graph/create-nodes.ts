bc732a511091d0082ffc02adfaafa08ebaab17e7
Tine Kondo <kondotine@gmail.com>
2023-12-28 00:00:19 +0000

    chore: fix typo

--- packages/nx-ktor/src/graph/create-nodes.ts
+++ packages/nx-ktor/src/graph/create-nodes.ts
@@ -1,5 +1,5 @@
 import { JVM_PROJECT_FILES, createNodesFor } from "@nxrocks/common-jvm";
 import { NX_KTOR_PKG } from "../index";
 
-// wrapped iton a () to avoid the 'Cannot access 'NX_KTOR_PKG' before initialization'
+// wrapped into a () to avoid the 'Cannot access 'NX_KTOR_PKG' before initialization'
 export const createNodesFn = () => createNodesFor(JVM_PROJECT_FILES, NX_KTOR_PKG); 
--- packages/nx-quarkus/src/graph/create-nodes.ts
+++ packages/nx-quarkus/src/graph/create-nodes.ts
@@ -1,5 +1,5 @@
 import { JVM_PROJECT_FILES, createNodesFor } from "@nxrocks/common-jvm";
 import { NX_QUARKUS_PKG } from "../index";
 
-// wrapped iton a () to avoid the 'Cannot access 'NX_QUARKUS_PKG' before initialization'
+// wrapped into a () to avoid the 'Cannot access 'NX_QUARKUS_PKG' before initialization'
 export const createNodesFn = () => createNodesFor(JVM_PROJECT_FILES, NX_QUARKUS_PKG); 
