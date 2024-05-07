be847ff737f77324c6f32550f33a88f2e8b9b9dc
Tine Kondo <kondotine@gmail.com>
2024-05-07 06:25:26 +0000

    chore: update to Nx workspace to `v19.0.1`

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Install pnpm
-npm install -g @pnpm/exe@8.3.1
+# Install pnpm.
+npm install -g @pnpm/exe@9.1.0
 
 
 # Make sure 'node' user can access the 'node_modules' folder that will be mounted as a volume
