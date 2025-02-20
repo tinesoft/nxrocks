b8baeb825e3f61abdf75e2b0e7c29058c0293b36
Tine Kondo <kondotine@gmail.com>
2025-02-20 12:58:06 +0000

    chore: migrate to bun

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,19 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# Uninstall globally installed PNPM (required version will be reinstalled through corepack)
-echo "❌ Uninstalling globally installed PNPM..."
-npm uninstall -g pnpm
+# Installing Bun
+echo "⚙️ Installing bun..."
+curl -fsSL https://bun.sh/install | bash
 
-# Prevent corepack from prompting user before downloading PNPM 
-export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
 
-# Enable corepack 
-corepack enable 
-
-# Install the PNPM version defined in the root package.json
-echo "⚙️ Installing required PNPM version..."
-corepack prepare --activate
+# Adding Bun to path
+echo 'export PATH="$HOME/.bun/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 
 # Install NPM dependencies
 echo "⚙️ Installing NPM dependencies..."
-pnpm install --frozen-lockfile
+bun install --frozen-lockfile
