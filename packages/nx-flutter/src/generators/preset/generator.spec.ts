66f3dcd3ce85927a5b3850a6bc73fd0f12fb1951
Tine Kondo <kondotine@gmail.com>
2025-03-27 17:11:00 +0000

    chore: migrate to ts project reference, new eslint config, and swc compiler

--- packages/nx-flutter/src/generators/preset/generator.spec.ts
+++ packages/nx-flutter/src/generators/preset/generator.spec.ts
@@ -6,8 +6,12 @@ import { PresetGeneratorSchema } from './schema';
 
 jest.mock('child_process'); // we need to mock 'execSync' so that it doesn't really run 'flutter' (reserved to e2e testing) (see __mocks__/child_process.js)
 
-//jest.mock('enquirer'); // we mock 'enquirer' to bypass the interactive prompt
 import * as enquirer from 'enquirer';
+
+// Mock enquirer's prompt
+jest.mock('enquirer', () => ({
+  prompt: jest.fn(),
+}));
 
 process.env['NX_INTERACTIVE'] = 'true'; // simulate normal cli interactive mode (the prompt is mocked anyway)
 
--- packages/nx-ktor/jest.config.ts
+++ packages/nx-ktor/jest.config.ts
@@ -1,10 +1,21 @@
 /* eslint-disable */
+import { readFileSync } from 'fs';
+
+// Reading the SWC compilation config for the spec files
+const swcJestConfig = JSON.parse(
+  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+);
+
+// Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
+swcJestConfig.swcrc = false;
+
 export default {
   displayName: 'nx-ktor',
   preset: '../../jest.preset.js',
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/nx-ktor',
+  coverageDirectory: 'test-output/jest/coverage',
 };
